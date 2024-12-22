6cb015dfcc99b4f57bf917c913a2086b3cf8c878
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-12-22 17:06:23 -0300

    criação do header

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import AppProvider from './hooks/index.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
+     
       <RouterProvider router={router} />
       <GlobalStyles />
       <ToastContainer autoClose={2000} theme='colored' />
