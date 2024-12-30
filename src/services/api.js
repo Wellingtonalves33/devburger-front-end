395911174effd5de98951f3517e46bfa1512671b
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-12-30 01:32:20 -0300

    ajusta configuração CORS e axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,  
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    }
 });
 
 api.interceptors.request.use((config) => {
     const userData = localStorage.getItem("devburger:userData");
     const token = userData && JSON.parse(userData).token;
-    config.headers.authorization = `Bearer ${token}`;
+    
+    if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+    }
+    
     return config;
 });
+
+// Adicionar interceptor de resposta para debug
+api.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('API Error:', {
+            config: error.config,
+            response: error.response
+        });
+        return Promise.reject(error);
+    }
+);
