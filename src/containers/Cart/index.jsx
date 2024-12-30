92b144dd4f447c393f3dd4c823efa638639aa2ad
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-12-30 00:38:31 -0300

    test

--- src/containers/Cart/index.jsx
+++ src/containers/Cart/index.jsx
@@ -1,12 +1,12 @@
 import { Container, Banner, Title, Content } from "./styles";
-import Logo from '../../assets/logo.svg'
+import LogoPhoto from '../../assets/Logo.svg'
 import { CartItems, CartResume } from "../../components";
 
 export function Cart() {
     return (
         <Container>
             <Banner>
-            <img src={Logo} alt='logo'/>
+            <img src={LogoPhoto} alt='logo'/>
             </Banner>
             <Title> ChecKout - Pedido </Title>
             <Content>      
