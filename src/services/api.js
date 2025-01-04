237f9407e50f1c0fe34781b363c423673d053ba9
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-01-04 08:10:35 -0300

    troquei a url

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,16 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    },
-    withCredentials: true // Adicionando esta configuração
+  baseURL: "https://dervburguer-api-production.up.railway.app/",
+  
 });
 
 api.interceptors.request.use((config) => {
-    const userData = localStorage.getItem("devburger:userData");
-    const token = userData && JSON.parse(userData).token;
+  const userData = localStorage.getItem("devburger:userData");
 
-    if (token) {
-        config.headers.authorization = `Bearer ${token}`;
-    }
+  const token = userData && JSON.parse(userData).token;
 
-    // Adicionando headers CORS específicos
-    config.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
-    return config;
+  config.headers.authorization = `Bearer ${token}`;
+
+  return config;
 });
-
-api.interceptors.response.use(
-    response => response,
-    error => {
-        console.error('API Error:', {
-            config: error.config,
-            response: error.response
-        });
-        return Promise.reject(error);
-    }
-);
